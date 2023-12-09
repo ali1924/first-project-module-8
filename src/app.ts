39ffcc07656843da2480afe6702e150a54953029
ali1924 <mohammadali229987@gmail.com>
2023-12-09 20:14:58 +0600

    8-3 Installing eslint, refactor code, fix errors using command

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
-const express = require('express')
-const app = express()
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+const app: Application = express();
 
+// parser
+app.use(express.json());
+app.use(cors());
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-export default app
+export default app;
--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -3,3 +3,8 @@ import path from "path";
 // current working directory
 // console.log(process.cwd());
 dotenv.config({ path: path.join(process.cwd(), ".env") });
+
+export default {
+  port: process.env.PORT,
+  database_url: process.env.DATABASE_URL,
+};
